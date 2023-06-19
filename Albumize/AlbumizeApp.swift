add2dac57de17df0feaadc5555f44a3570106dbc
h-kanta <uta_082812@icloud.com>
2023-06-19 23:07:09 +0900

    Initial Commit

---
+++ Albumize/AlbumizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumizeApp.swift
+//  Albumize
+//
+//  Created by 堀川貫太 on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct AlbumizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
