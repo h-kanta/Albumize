4031b3376f7f5ec772c8207e58f71a27d5f9b50d
h-kanta <uta_082812@icloud.com>
2023-07-30 17:39:54 +0900

    FirebaseAuthentication導入

--- Albumize/AlbumizeApp.swift
+++ Albumize/AlbumizeApp.swift
@@ -6,11 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct AlbumizeApp: App {
 //    let photo = Photo()
 //    let album = Album()
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
 //            ContentView(photoViewModel: .init(photo: photo))
--- Podfile
+++ Podfile
@@ -6,5 +6,15 @@ target 'Albumize' do
   use_frameworks!
 
   # Pods for Albumize
-
+  
+  # Firebase
+  pod 'FirebaseAuth'
+  
+  post_install do |installer|
+    installer.pods_project.targets.each do |target|
+      target.build_configurations.each do |config|
+        config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '11.0'
+      end
+    end
+  end
 end
